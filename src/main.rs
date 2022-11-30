55d47dde6fde10a8b063116af8cb721ec1be707c
3tilley <1138504+3tilley@users.noreply.github.com>
2022-11-30 14:14:47 +0000

    Inventory

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,64 @@
-use seed::window;
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
+use seed::{prelude::*, *};
 
-fn main() {
-    window().alert_with_message("Hello, world!");
+struct Model {
+    inventory: HashMap<String, u32>,
 }
+
+impl Model {
+    fn new() -> Self {
+        Self {
+            inventory: HashMap::new(),
+        }
+    }
+}
+
+enum Msg {
+    AddItem(String),
+    RemoveItem(String),
+}
+
+fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+    Model::new()
+}
+
+fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::AddItem(item) => {
+            let count = model.inventory.entry(item.to_uppercase()).or_insert(0);
+            *count += 1;
+        }
+        Msg::RemoveItem(item) => {
+            let count = model.inventory.entry(item.to_uppercase());
+            match count {
+                //Entry::Occupied(c) if *c. == 0 => panic!("No items to remove"),
+                Entry::Occupied(c) => {
+                    let mut count = c.get();
+                    //*count += 1
+                }
+                Entry::Vacant(_) => panic!("No items found in inventory")
+            }
+        }
+    }
+}
+
+
+
+fn view(model: &Model) -> Node<Msg> {
+    div![
+        "This is a counter: ",
+        C!["counter"],
+        button![
+            "Add",
+            ev(Ev::Click, |_| Msg::AddItem("Dog".to_string())),
+        ],
+    ]
+}
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    App::start("app", init, update, view);
+}
+
+fn main() {}
